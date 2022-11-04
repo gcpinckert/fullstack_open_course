c0faa466cea17d1c1c7d196407a74d692b592bfd
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-04 14:19:40 -0700

    Lesson 1c: tutorial

--- part1/tutorial/src/App.js
+++ part1/tutorial/src/App.js
@@ -1,26 +1,39 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from 'react';
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app was created by <em>ME</em>
-    </div>
-  )
-}
+// simplify props.counter by leveraging destructuring assignment
+// use the compact form of arrow functions
+const Display = ({ counter }) => <div>{counter}</div>
+
+// simplify props.onClick and props.text with destructuring assignment
+// use the compact form of arrow functions
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  const decreaseByOne = () => setCounter(counter - 1);
+
   return (
-    <>
-      <h1>Greetings</h1>,
-      <Hello name='Ginni' age={24 + 10}/>,
-      <Footer />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
   )
 }
 
