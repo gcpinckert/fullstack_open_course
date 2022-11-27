f9b2fde04b5dce6f8867810bdbc21a9c2c2a393f
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-27 11:31:32 -0800

    Part 5d: End to End Testing --revisit

---
+++ part4/notes_frontend/cypress/e2e/note_app.cy.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
+  })
+
+  it('login form can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('login').click()
+  })
+})
