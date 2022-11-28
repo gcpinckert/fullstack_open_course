86ff1760b3b08f5522b846b2cc70556179c48800
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-28 14:00:23 -0800

    Part 6c: Redux to Backend Communication

--- part6/notes/src/index.js
+++ part6/notes/src/index.js
@@ -5,14 +5,19 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
     notes: noteReducer,
     filter: filterReducer,
   }
+})
+
+noteService.getAll().then(notes => {
+  store.dispatch(setNotes(notes))
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
